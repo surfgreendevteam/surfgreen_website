da75558ee710410ae858001824f685eed5a9cc9e
Carl Bednorz <carl@surfgreen.dev>
2023-05-19 12:23:21 +0200

    update

--- surfgreen_app/content_marketing/cms_plugins.py
+++ surfgreen_app/content_marketing/cms_plugins.py
@@ -2,7 +2,13 @@ from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from django.utils.translation import gettext as _
 
-from surfgreen_app.content_marketing.models import HeroServiceModule, OfferServiceItem, OfferServiceModule
+from surfgreen_app.content_marketing.models import (
+    HeroServiceModule,
+    HowWeWorkItem,
+    HowWeWorkModule,
+    OfferServiceItem,
+    OfferServiceModule,
+)
 
 
 @plugin_pool.register_plugin  # register the plugin
@@ -36,6 +42,32 @@ class OfferServiceItemPublisher(CMSPluginBase):
     module = _("Offer Service Item")
     name = _("Offer Service Item Plugin")
     render_template = "content_marketing/offer_service_item.html"
+    require_parent = True
+
+    def render(self, context, instance, placeholder):
+        context = super().render(context, instance, placeholder)
+        return context
+
+
+@plugin_pool.register_plugin
+class HowWeWorkModulePublisher(CMSPluginBase):
+    model = HowWeWorkModule
+    module = _("How we work Module")
+    name = _("How we work Module Plugin")
+    render_template = "content_marketing/howwework_service_module.html"
+    allow_children = True
+
+    def render(self, context, instance, placeholder):
+        context = super().render(context, instance, placeholder)
+        return context
+
+
+@plugin_pool.register_plugin
+class HowWeWorkItemPublisher(CMSPluginBase):
+    model = HowWeWorkItem
+    module = _("How we work Item")
+    name = _("How we work Item Plugin")
+    render_template = "content_marketing/howwework_service_item.html"
     require_parent = True
 
     def render(self, context, instance, placeholder):
--- surfgreen_app/content_marketing/models.py
+++ surfgreen_app/content_marketing/models.py
@@ -29,3 +29,22 @@ class OfferServiceItem(CMSPlugin):
 
     def __str__(self):
         return self.title
+
+
+class HowWeWorkModule(CMSPlugin):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    link = models.CharField(max_length=255, blank=True, null=True)
+    link_title = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class HowWeWorkItem(CMSPlugin):
+    step_number = models.CharField(max_length=255, blank=True, null=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
