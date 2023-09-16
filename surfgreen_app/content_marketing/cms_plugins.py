20eb0a6fa31b91148c72f305de6a003a6906022e
Carl Bednorz <carl@surfgreen.dev>
2023-09-16 12:24:18 +0200

    Home page About Module update

--- surfgreen_app/content_marketing/cms_plugins.py
+++ surfgreen_app/content_marketing/cms_plugins.py
@@ -50,9 +50,9 @@ class HeroServiceModulePublisher(CMSPluginBase):
 @plugin_pool.register_plugin  # register the plugin
 class HomePageHeroModulePublisher(CMSPluginBase):
     model = HomePageHeroModule  # model where plugin data are saved
-    module = _("Home Page About Module")
-    name = _("Home Page About Plugin")  # name of the plugin in the interface
-    render_template = "content_marketing/homepage_about_module.html"
+    module = _("Home Page Hero Module")
+    name = _("Home Page Hero Plugin")  # name of the plugin in the interface
+    render_template = "content_marketing/homepage_hero.html"
 
     def render(self, context, instance, placeholder):
         context.update({"instance": instance})
@@ -62,9 +62,9 @@ class HomePageHeroModulePublisher(CMSPluginBase):
 @plugin_pool.register_plugin  # register the plugin
 class HomePageAboutModulePublisher(CMSPluginBase):
     model = HomepageAboutModule  # model where plugin data are saved
-    module = _("Home Page Hero Module")
-    name = _("Home Page Hero Plugin")  # name of the plugin in the interface
-    render_template = "content_marketing/homepage_hero.html"
+    module = _("Home Page About Module")
+    name = _("Home Page About Plugin")  # name of the plugin in the interface
+    render_template = "content_marketing/homepage_about_module.html"
 
     def render(self, context, instance, placeholder):
         context.update({"instance": instance})
