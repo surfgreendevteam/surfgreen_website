b1f27a9bb3807bc5c86a88119a747b5fd2c6adf8
Carl Bednorz <carl@surfgreen.dev>
2024-11-03 10:52:48 +0100

    Added csrf trusted origin settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -240,6 +240,7 @@ FIXTURE_DIRS = (str(APPS_DIR / "fixtures"),)
 SESSION_COOKIE_HTTPONLY = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-httponly
 CSRF_COOKIE_HTTPONLY = True
+CSRF_TRUSTED_ORIGINS = ['https://surfgreen.dev', 'https://www.surfgreen.dev']
 # https://docs.djangoproject.com/en/dev/ref/settings/#x-frame-options
 # X_FRAME_OPTIONS = "DENY"
 X_FRAME_OPTIONS = "SAMEORIGIN"  # required by django cms
